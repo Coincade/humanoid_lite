1b223d137c8f5ded235c020927393affd975c334
YashKVR <yashincrypto5@gmail.com>
2025-08-08 14:58:50 +0530

    Add Mujoco FIles

--- README.md
+++ README.md
@@ -202,7 +202,7 @@ This project is licensed under the MIT License. See LICENSE file for details.
 
 ## Contributing
 
-1. Fork the repository
-2. Create a feature branch
-3. Make your changes
-4. Submit a pull request
+1. Fork the repository.
+2. Create a feature branch.
+3. Make your changes.
+4. Submit a pull request.
--- source/humanoid_lite_lowlevel/humanoid_lite_lowlevel/policy/config.py
+++ source/humanoid_lite_lowlevel/humanoid_lite_lowlevel/policy/config.py
@@ -56,7 +56,7 @@ class Cfg(DictConfig):
         parser.add_argument(
             "--config",
             type=str,
-            default="./configs/policy_biped.yaml",
+            default="./configs/policy_humanoid_lite.yaml",
             help="Path to the configuration file",
         )
         args = parser.parse_args()
